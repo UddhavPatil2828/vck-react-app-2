cffcc33bb5cf1b12ad010cf90a1e26a70db94d13
Uddhav Patil <patiluddhav68@gmail.com>
2025-06-27 16:35:51 +0530

    header.css updated

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import './header.css'
+import './Header.css'
 import {Link} from 'react-router-dom'
 const Header =()=>{
   const [menuOpen, setMenuOpen] = useState(false);
